d896c71c59fbe991c82aad935b8c6995f521e39a
Github-2lu <101921581+Github-2lu@users.noreply.github.com>
2024-06-23 20:10:44 +0530

    Update 12.grub.sh

--- base_arch/scripts/12.grub.sh
+++ base_arch/scripts/12.grub.sh
@@ -1,11 +1,11 @@
-drive=$(grep -oP "Drive=\K.*" /scripts/install.config)
-efi_drive=$drive
-efi_drive+='1'
+# drive=$(grep -oP "Drive=\K.*" /scripts/install.config)
+# efi_drive=$drive
+# efi_drive+='1'
 
 printf "y" | pacman -S grub efibootmgr dosfstools os-prober mtools
-mkdir -v /boot/EFI
-mount $efi_drive /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+# mkdir -v /boot/EFI
+# mount $efi_drive /boot/EFI
+grub-install --target=x86_64-efi /boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
 unset efi_drive
